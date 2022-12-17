29bb513d11f789b31aabf267e32b6775f18ab520
scrawnyColeman <thomashutchinson450@gmail.com>
2022-12-17 14:30:03 +0000

    removed unnecessary prop

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,18 +1,12 @@
 import React, { FunctionComponent } from "react";
 
 import { Layout, LinkedIn, SEO, Typewriter } from "../components";
-import { LINKEDIN_URL } from "../constants";
 
 const HomeView: FunctionComponent = () => (
   <Layout>
     <SEO />
     <Typewriter />
-
-    <LinkedIn
-      handleAction={() => {
-        window.open(LINKEDIN_URL, "Odhran's LinkedIn");
-      }}
-    />
+    <LinkedIn />
   </Layout>
 );
 
